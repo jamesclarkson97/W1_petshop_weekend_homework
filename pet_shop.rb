67adcbffd819ef8c58670177adfa4ddc66de8213
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-06-19 16:22:20 +0100

    test add cash pass

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,8 @@ end
 def total_cash(shop_name)
     return shop_name[:admin][:total_cash]
 end
+
+def add_or_remove_cash(shop_name, value)
+    shop_name[:admin][:total_cash] += value
+    return total_cash(shop_name)
+end
